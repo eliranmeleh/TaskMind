e9f90cc505cc89c15a28bbf2a73071844af0b99b
Miron-Hanukaiev <kcamiron34@gmail.com>
2025-10-14 12:50:40 +0300

    Express-start

--- server/index.js
+++ server/index.js
@@ -1,22 +1,13 @@
-// טוען משתני סביבה (אם נשתמש בהם בהמשך)
-require('dotenv').config();
-
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
+import express from "express";
 
 const app = express();
-
-app.use(cors());        
-app.use(express.json());  
-app.use(morgan('dev'));   
+const port = 4000;
 
 app.get('/health', (req, res) => {
   res.json({ ok: true });
 });
 
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`🚀 Server running on http://localhost:${port}`);
 });
+
