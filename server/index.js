65ce6bc4a4bac44356cc08b1423f58bfd70eaace
Miron-Hanukaiev <kcamiron34@gmail.com>
2025-10-15 12:01:53 +0300

    Added delete task and update task

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 import express from "express";
 
+import tasksRouter from "./routes/tasks.routes.js";
+
 const app = express();
+app.use(express.json()); // ⬅️ שורה קריטית!
+
 const port = 4000;
+
 
 app.get('/health', (req, res) => {
   res.json({ ok: true });
 });
+
+app.use("/api/tasks", tasksRouter);
 
 app.listen(port, () => {
   console.log(`🚀 Server running on http://localhost:${port}`);
--- server/routes/tasks.routes.js
+++ server/routes/tasks.routes.js
@@ -1,5 +1,4 @@
 import express from "express";
-
 const router = express.Router();
 
 let tasks = [];
@@ -9,10 +8,11 @@ router.get("/", (req, res) => {
   res.json({ msg: "📋 All tasks will be listed here" });
 });
 
+// Adding a new task
 router.post("/", (req, res) => {
   const { title } = req.body; 
   if (!title) {
-    return res.status(400).json({ error: "חסר שם משימה" });
+    return res.status(400).json({ error: "The name of the task misses"});
   }
 
   const newTask = {
@@ -25,5 +25,36 @@ router.post("/", (req, res) => {
   res.status(201).json(newTask);
 });
 
+// Deleting an existing task
+router.delete("/", (req, res) => {
+  const { title } = req.body;
+  if (!title) {
+    return res.status(400).json({ error: "The name of the task misses"});
+  }
+  
+  const index = tasks.findIndex(t => t.title === title);
+  if (index === -1) {
+    return res.status(404).json({ error: "There is no task with the matching name"});
+  }
+
+  tasks[index].delete;
+  res.status(201).json("Deleted successfully");
+})
+
+// Updating a task to have a status "done"
+router.put("/", (req, res) => {
+  const { title } = req.body;
+  if (!title) {
+    return res.status(400).json({ error: "The name of the task misses"});
+  }
+
+  const index = tasks.findIndex(t => t.title === title);
+  if (index === -1) {
+    return res.status(404).json({ error: "There is no task with the matching name"});
+  }
+
+  tasks[index].status = "done";
+  res.status(201).json(tasks[index]);
+})
 
 export default router;
