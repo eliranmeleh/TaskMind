5f0fd52197e8df0f38116f894026f754f776c35d
Miron-Hanukaiev <kcamiron34@gmail.com>
2025-10-20 14:52:57 +0300

    Added a connection to mongodb + made a Task file

---
+++ server/db/connects.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const uri = "mongodb+srv://Mirmir:Aa123456@cluster0.tvyxxcg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB Atlas");
+  } catch (err) {
+    console.error("MongoDB connection failed:", err.message);
+  }
+};
+
+export default connectToDB;
--- server/index.js
+++ server/index.js
@@ -2,20 +2,31 @@ import express from "express";
 import bodyParser from "body-parser";
 
 import tasksRouter from "./routes/tasks.routes.js";
+import connectToDB from "./db/connects.js";
 
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true}));
 
 const port = 4000;
 
+const startServer = async () => {
+  try {
+    await connectToDB();
+    app.listen(port, () => {
+      console.log(`Server running on http://localhost:${port}`);
+    });
+  } catch (err) {
+    console.error("Failed to connect to MongoDB:", err.message);
+    process.exit(1);
+  }
+};
+
+startServer();
 app.get('/health', (req, res) => {
   res.json({ ok: true });
 });
 
 app.use("/api/tasks", tasksRouter);
-
-app.listen(port, () => {
-  console.log(`🚀 Server running on http://localhost:${port}`);
-});
 
 
